6dc8fd2e6e4fa31b54459bf8fbefa0e842c98ffb
zequizavala <zequizavala93@gmail.com>
2018-11-29 00:20:29 -0300

    nordelta

---
+++ js/map.js
@@ -0,0 +1,7 @@
+document.getElementById('yatch').addEventListener("mouseover", function(){
+    document.getElementById('yatch-text').classList.add('show')
+})
+
+document.getElementById('yatch').addEventListener("mouseout", function(){
+    document.getElementById('yatch-text').classList.add('show')
+})
